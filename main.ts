b6109598f1c858a900124738a2479942d8a34358
elsymeis <56837582+elsymeis@users.noreply.github.com>
2022-02-03 16:24:03 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,13 @@ basic.showIcon(IconNames.Yes)
 let strip = neopixel.create(DigitalPin.P12, 5, NeoPixelMode.RGB)
 strip.setBrightness(50)
 basic.forever(function () {
-    if (gatorMicrophone.getSoundIntensity() > 100) {
-        strip.setBrightness(50)
+    if (gatorMicrophone.getSoundIntensity() > 200) {
         basic.clearScreen()
+        strip.setBrightness(50)
         strip.showBarGraph(gatorMicrophone.getSoundIntensity(), 2000)
-    } else {
         basic.showIcon(IconNames.Sad)
-        strip.setBrightness(0)
+    } else {
+        basic.showIcon(IconNames.Happy)
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
     }
 })

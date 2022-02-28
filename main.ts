8ca422dffdff776b241d3e8cd468d7189fbbd98a
elsymeis <56837582+elsymeis@users.noreply.github.com>
2022-02-28 11:28:13 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ basic.showIcon(IconNames.Yes)
 let strip = neopixel.create(DigitalPin.P12, 5, NeoPixelMode.RGB)
 strip.setBrightness(50)
 basic.forever(function () {
-    if (gatorMicrophone.getSoundIntensity() > 200) {
+    if (gatorMicrophone.getSoundIntensity() > 2000) {
         basic.clearScreen()
         strip.setBrightness(50)
         strip.showBarGraph(gatorMicrophone.getSoundIntensity(), 2000)
